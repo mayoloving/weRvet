9b443ab22ed07f3d7ffead1acbf5896af4201adc
mayoloving <yota.benz@outlook.com>
2023-01-15 12:59:54 +0200

    changing app

--- app/weRvet.py
+++ app/weRvet.py
@@ -2,7 +2,6 @@ from flask import Flask, render_template, request
 from datetime import datetime
 from flask_sqlalchemy import SQLAlchemy
 import os
-# from flask_mysqldb import MySQL
 import mysql.connector
 
 
@@ -11,50 +10,36 @@ app = Flask(__name__)
 app.secret_key = "yotam"
 
 
-# app.config['MYSQL_HOST'] = 'localhost'
-# app.config['MYSQL_USER'] = 'root'
-# app.config['MYSQL_PASSWORD'] = ''
-# app.config['MYSQL_DB'] = 'flask'
- 
-# mysql = MySQL(app)
-
-
 @app.route("/")
 def home():
-    if not os.path.isfile(f"templates/general.txt"):
-        file = open(f"templates/general.txt", 'a')
-        # cursor = mysql.connection.cursor()
-        # cursor.execute(''' CREATE TABLE general(name, message) ''')
-        # mysql.connection.commit()
-        # cursor.close()
     return render_template('index.html')
 
-@app.route("/<room>")
-def rooms(room):
-    if not os.path.isfile(f"templates/{room}.txt"):
-        file = open(f"templates/{room}.txt", 'a')
-        # cursor = mysql.connection.cursor()
-        # cursor.execute(''' CREATE TABLE {room}(name, message) ''')
-        # mysql.connection.commit()
-        # cursor.close()
+@app.route("/metrics")
+def metrics():
+    return render_template('index.html')
+
+@app.route("/pet")
+def pets_ids():
     return render_template('index.html')
 
 
-@app.route("/api/chat/<room>", methods=["POST","GET"])
-def post_chat(room):
+@app.route("/pet/<id>", methods=["POST","GET","DELETE","PUT"])
+def specs_pet(id):
     if request.method == "POST":
-        name = request.form["username"]
-        message = request.form["msg"]
-        file = open(f"templates/{room}.txt", 'a')
-        file.write(f"{datetime.now()} >> {name}:{message}\n")
-        # cursor = mysql.connection.cursor()
-        # cursor.execute(''' INSERT INTO {room} VALUES(%s,%s)''',(name,message))
-        # mysql.connection.commit()
-        # cursor.close()
-        return message
-    else:
-        return render_template(f"{room}.txt")
+        # `POST /pet/{id}` with body containing some details as JSON.
+        return render_template('index.html')
 
+    elif request.method == "GET":
+        # `GET /pet/{id}` to return specific JSON object.
+        return render_template('index.html')
+
+    elif request.method == "DELETE":
+        # `DELETE /pet/{id}` remove an entity from database.
+        return render_template('index.html')
+
+    else:
+        # `PUT /pet/{id}` same thing exactly, but updates existing pet.
+        return render_template('index.html')
 
 if __name__ == "__main__":
     app.run(debug=True, host="0.0.0.0", port=80)
