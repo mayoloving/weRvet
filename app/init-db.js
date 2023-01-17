f21848c701341dfbacc09fe1ce5794dfb1dcc9f1
mayoloving <yota.benz@outlook.com>
2023-01-17 22:20:50 +0200

    small bugs2

--- app/init-db.js
+++ app/init-db.js
@@ -4,14 +4,14 @@ db.pets_tb.drop()
 db.pets_tb.insertMany([
     {
         "name": "blanco",
-        "id": 1,
+        "id": "1",
         "gender": "M",
         "type": "dog",
         "message": "he is cute"
     },
     {
         "name": "rexy",
-        "id": 2,
+        "id": "2",
         "gender": "F",
         "type": "cat",
         "message": "wowza"
