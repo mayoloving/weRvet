061fef3edc525b59a351962a16b654f4330537ce
mayoloving <yota.benz@outlook.com>
2023-01-22 23:38:23 +0200

    deploy stage 4

--- calc_tag.sh
+++ calc_tag.sh
@@ -1,18 +1,18 @@
 #!/bin/bash
 
-t=$(git tag -l | tail -1)
-major=$(git tag -l | tail -1 | cut -d"." -f"1")
-minor=$(git tag -l | tail -1 | cut -d"." -f"2")
-line=$(git tag -l | wc -l)
+t=$(git tag -l | sort -V | tail -1)
+major=$(git tag -l | sort -V | tail -1 | cut -d"." -f"1")
+minor=$(git tag -l | sort -V | tail -1 | cut -d"." -f"2")
+line=$(git tag -l | sort -V | wc -l)
 s=$major.$minor
 
-if [ -z "$(git tag -l)" ];
+if [ -z "$(git tag -l | sort -V)" ];
 then
     echo "no tags were entered yet by coders"
 elif [ "$line" == "1" ] && [ "$t" == "$s" ];
 then
-    major=$(git tag -l | tail -1 | cut -d"." -f"1")
-    minor=$(git tag -l | tail -1 | cut -d"." -f"2")
+    major=$(git tag -l | sort -V | tail -1 | cut -d"." -f"1")
+    minor=$(git tag -l | sort -V | tail -1 | cut -d"." -f"2")
     val=$major.$minor.1
 
     # git tag -d $major.$minor
@@ -21,14 +21,14 @@ then
     git tag $val HEAD
     git push https://github.com/mayoloving/weRvet.git $val
 else
-    majorlast=$(git tag -l | tail -1 | cut -d"." -f"1")
-    minorlast=$(git tag -l | tail -1 | cut -d"." -f"2")
-    major=$(git tag -l | tail -2 | head -1 | cut -d"." -f"1")
-    minor=$(git tag -l | tail -2 | head -1 | cut -d"." -f"2")
+    majorlast=$(git tag -l | sort -V | tail -1 | cut -d"." -f"1")
+    minorlast=$(git tag -l | sort -V | tail -1 | cut -d"." -f"2")
+    major=$(git tag -l | sort -V | tail -2 | head -1 | cut -d"." -f"1")
+    minor=$(git tag -l | sort -V | tail -2 | head -1 | cut -d"." -f"2")
 
     if [ "$major" == "$majorlast" ] && [ "$minor" == "$minorlast" ];
     then
-        num=$(git tag -l | tail -1 | cut -d"." -f"3")
+        num=$(git tag -l | sort -V | tail -1 | cut -d"." -f"3")
         num=$(($num+1))
         val=$major.$minor.$num
         echo "2"
